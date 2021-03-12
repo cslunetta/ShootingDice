8f06694f398c8e80679d7718a8fe477cbd8e5015
cslunetta <cslunetta@outlook.com>
2021-03-12 15:04:13 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShootingDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
